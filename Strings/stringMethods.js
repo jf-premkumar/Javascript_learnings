5e7583952a59e8921fa2cc201e37382b6f398953
jf-premkumar <premuhulikoppe@gmail.com>
2022-08-30 12:17:35 +0530

    fromCodePoint(), includes(), indexOf(), lastIndexOf(), match(), matchAll(), normalize()

--- Strings/stringMethods.js
+++ Strings/stringMethods.js
@@ -58,3 +58,73 @@ console.log(`Character at index: ${3} is ${animal.charCodeAt(3)}`)
 // 5) String.prototype.codePointAt()
 const icons = '☃★♲';
 console.log('code point at index 1 is' , icons.codePointAt(1))
+
+// 6) String.prototype.concat() 
+const str1 = 'Premkumar';
+const str2 = 'Hulikoppe';
+console.log('resultant string', str1.concat(' ', str2));
+
+// 7) String.prototype.endsWith()
+const str3 = 'Cats are the best!';
+console.log(str3.endsWith('best!'));
+
+// 8) String.fromCharCode()
+console.log(String.fromCharCode(100, 101, 151, 272));
+
+// 9) String.fromCodePoint()
+console.log(String.fromCodePoint(8088, 9327, 9152));
+
+// 10) String.prototype.includes()
+const stringLine = 'Premkumar is at Noida currently, working as Software developer';
+console.log(stringLine.includes('Noida'));
+
+// 11) String.prototype.indexOf()
+const paragraph = 'I am Premkumar hulikoppe is Premkumar';
+console.log(paragraph.indexOf('Premkumar'));
+
+// 12) String.prototype.lastIndexOf()
+console.log(paragraph.lastIndexOf('Premkumar'));
+
+// 13) String.prototype.lastIndexOf()
+const a = 'réservé'; // with accents, lowercase
+const b = 'RESERVE'; // no accents, uppercase
+
+console.log(a.localeCompare(b));   // a comes after b so +ve
+console.log(b.localeCompare(a));    // b wont come after a so -ve
+console.log('check'.localeCompare('against'));
+console.log('check'.localeCompare('check'));   // both same so zero
+
+const items = ['réservé', 'Premier', 'Cliché', 'communiqué', 'café', 'Adieu'];
+items.sort((a, b) => a.localeCompare(b, 'fr', { ignorePunctuation: true }));
+// ['Adieu', 'café', 'Cliché', 'communiqué', 'Premier', 'réservé']
+console.log(items)
+
+// 14) String.prototype.match()
+const paragraph1 = 'The quick brown fox jumps over the lazy dog. It barked.';
+const regex = /[a-g]/g;
+const found = paragraph1.match(regex);
+console.log('found '  , found.sort());
+
+// 15)  String.prototype.matchAll()
+const regexp = /foo[a-z]*/g;
+const str = 'table football, foosball';
+const matches = str.matchAll(regexp);
+
+// console.log(`matches : ${matches}`);
+for (const iterator of matches) {
+    console.log(`Found ${iterator} in string`);
+}
+
+// 16) String.prototype.normalize()
+const name1 = '\u0041\u006d\u00e9\u006c\u0069\u0065';
+const name2 = '\u0041\u006d\u0065\u0301\u006c\u0069\u0065';
+console.log(`name1: ${name1} name2: ${name2}`);
+console.log(name1 === name2)
+console.log(name1.length == name2.length)
+
+const nameNFC1 = name1.normalize('NFC');
+const nameNFC2 = name2.normalize('NFC');
+console.log(`nameNFC1: ${nameNFC1} nameNFC2: ${nameNFC2}`);
+console.log(nameNFC1 === nameNFC2)
+console.log(nameNFC1.length == nameNFC2.length)
+
