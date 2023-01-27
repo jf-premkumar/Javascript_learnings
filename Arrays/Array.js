06657f73a3043edf334253cbccfd9961e15dfc4d
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-01-27 17:40:08 +0530

    started exploring Arrays

---
+++ Arrays/Array.js
@@ -0,0 +1,15 @@
+// The Array object, as with arrays in other programming languages, enables storing a collection of multiple items under a single variable name, and has members for performing common array operations.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+
+// Array Declarations:
+//1. Array literal notation
+const fruits = ["Apple", "Banana"];
+
+console.log(fruits.length); // 2
+console.log(fruits[0]); // "Apple"
+
+//2. Array constructor with a single parameter
+const fruits = new Array(2);
+
+console.log(fruits.length); // 2
+console.log(fruits[0]); // undefined
